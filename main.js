111f3ef0fa54c212097a80b25e7f636c178edee2
suah lee <suah216@gmail.com>
2023-03-30 22:09:10 +0900

    second

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//캔버스 세팅
 let canvas;
 let ctx;
 
@@ -7,3 +8,34 @@ canvas.width=400;
 canvas.height=700;
 document.body.appendChild(canvas);
 
+
+let spaceImage,spaceshipImage,bulletImage,enemyImage,gameImage;
+//우주선좌표 
+let spaceshipX
+let spaceshipY
+function loadImage(){
+    spaceImage = new Image();
+    spaceImage.src="images/space.jpg";
+
+    spaceshipImage = new Image();
+    spaceshipImage.src="images/spaceship.png"
+
+    bulletImage = new Image();
+    bulletImage.src="images/bullet.png"
+
+    enemyImage =new Image();
+    enemyImage.src="images/enemy.png"
+
+    gameImage= new Image();
+    gameImage.src="images/game.jpg"
+}
+
+function render(){
+    ctx.drawImage(spaceImage, 0, 0, canvas.width, canvas.height);
+}
+function main(){
+    render()
+    requestAnimationFrame(main)
+}
+loadImage();
+main();
