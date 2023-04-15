73079b53012570d356701515972c59a74177a519
suah lee <suah216@gmail.com>
2023-04-15 09:55:20 +0900

    shut finish

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ document.body.appendChild(canvas);
 
 let spaceImage,spaceshipImage,bulletImage,enemyImage,gameImage;
 let gameOver=false;
-
+let score=0;
 //우주선좌표 
 let spaceshipX=canvas.width/2-32;
 let spaceshipY=canvas.height-64;
@@ -23,12 +23,22 @@ function Bullet(){
     this.init=function(){
         this.x = spaceshipX + 20;
         this.y = spaceshipY;
-
+        this.alive=true;
         bulletList.push(this);
     };
     this.update=function(){
         this.y-=7;
     };
+    this.checkHit=function(){
+        for(let i=0; i<enemyList.length; i++){
+            if(this.y <= enemyList[i].y && this.x>=enemyList[i].x && this.x<=enemyList[i].x+40){
+                score++;
+                this.alive=false;
+                enemyList.splice(i,1);
+            }
+        }
+        
+    }
 }
 function generateRandomValue(min,max){
     let randomNum=Math.floor(Math.random()*(max-min+1))+min;//floor :내림 함수 
@@ -110,7 +120,10 @@ function update(){
         spaceshipX=canvas.width-64;
     }
     for(let i = 0; i<bulletList.length; i++){
-        bulletList[i].update();
+        if(bulletList[i].alive){
+            bulletList[i].update();
+            bulletList[i].checkHit();
+        }       
     }
     for(let i=0;i<enemyList.length;i++){
         enemyList[i].update();
@@ -122,7 +135,9 @@ function render(){
     ctx.drawImage(spaceshipImage,spaceshipX,spaceshipY);
     
     for(let i=0; i<bulletList.length; i++){
-        ctx.drawImage(bulletImage,bulletList[i].x,bulletList[i].y);
+        if(bulletList[i].alive){
+            ctx.drawImage(bulletImage,bulletList[i].x,bulletList[i].y);    
+        }
     }
     for(let i=0;i<enemyList.length;i++){
         ctx.drawImage(enemyImage,enemyList[i].x,enemyList[i].y);
